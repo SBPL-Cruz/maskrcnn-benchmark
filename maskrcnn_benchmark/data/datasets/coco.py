fixed all 0s coming in viewpoints and rotation

maskrcnn_benchmark/data/datasets/coco.py
@@ -95,7 +95,7 @@ class COCODataset(torchvision.datasets.coco.CocoDetection):
         if anno and "inplane_rotation_id" in anno[0]:
             inplane_rotation_id = [obj["inplane_rotation_id"] for obj in anno]
             target.add_field("inplane_rotations", torch.tensor(inplane_rotation_id))
-        # print(inplane_rotation_id)
+        # print(viewpoint_id)
 
         target = target.clip_to_image(remove_empty=True)
 

maskrcnn_benchmark/modeling/roi_heads/box_head/inference.py
@@ -56,10 +56,11 @@ class PostProcessor(nn.Module):
         """
         class_logits, box_regression, viewpoint_logits, inplane_rotation_logits = x
         class_prob = F.softmax(class_logits, -1)
-
         if self.pose_on:
-            viewpoint_prob = F.softmax(viewpoint_logits, -1)
-            inplane_rotation_prob = F.softmax(inplane_rotation_logits, -1)
+            # print(viewpoint_logits.shape)
+            viewpoint_prob = F.softmax(viewpoint_logits, dim=1)
+            inplane_rotation_prob = F.softmax(inplane_rotation_logits, dim=1)
+            # print(torch.argmax(viewpoint_prob, dim=1))
 
         # TODO think about a representation of batch of boxes
         image_shapes = [box.size for box in boxes]

maskrcnn_benchmark/modeling/roi_heads/box_head/loss.py
@@ -81,14 +81,14 @@ class FastRCNNLossComputation(object):
             ignore_inds = matched_idxs == Matcher.BETWEEN_THRESHOLDS
             labels_per_image[ignore_inds] = -1  # -1 is ignored by sampler
 
-            if matched_targets.has_field('viewpoints') \
-                and matched_targets.has_field('inplane_rotations'):
-                viewpoints_per_image[bg_inds] = 0
-                viewpoints_per_image[ignore_inds] = -1
-                viewpoints.append(viewpoints_per_image)
-                inplane_rotations_per_image[bg_inds] = 0
-                inplane_rotations_per_image[ignore_inds] = -1
-                inplane_rotations.append(inplane_rotations_per_image)
+            # if matched_targets.has_field('viewpoints') \
+            #     and matched_targets.has_field('inplane_rotations'):
+            #     viewpoints_per_image[bg_inds] = 0
+            #     viewpoints_per_image[ignore_inds] = -1
+            viewpoints.append(viewpoints_per_image)
+            #     inplane_rotations_per_image[bg_inds] = 0
+            #     inplane_rotations_per_image[ignore_inds] = -1
+            inplane_rotations.append(inplane_rotations_per_image)
 
 
             # compute regression targets
@@ -196,6 +196,10 @@ class FastRCNNLossComputation(object):
             viewpoints = cat([proposal.get_field("viewpoints") for proposal in proposals], dim=0)
             inplane_rotations = cat([proposal.get_field("inplane_rotations") for proposal in proposals], dim=0)
             
+            # print(viewpoints)
+            # viewpoint_prob = F.softmax(viewpoint_logits, dim=1)
+            # print(torch.argmax(viewpoint_prob, dim=1))
+
             viewpoint_loss = F.cross_entropy(viewpoint_logits, viewpoints)
             inplane_rotation_loss = F.cross_entropy(inplane_rotation_logits, inplane_rotations)
 

tools/fat_dataset/fat_pose_image.py
@@ -36,7 +36,7 @@ print(all_predictions[:5])
 category_ids = example_coco.getCatIds(catNms=['square'])
 image_ids = example_coco.getImgIds(catIds=category_ids)
 # image_data = example_coco.loadImgs(image_ids[np.random.randint(0, len(image_ids))])[0]
-image_data = example_coco.loadImgs(image_ids[0])[0]
+image_data = example_coco.loadImgs(image_ids[100])[0]
 viewpoints_xyz = np.array(example_coco.dataset['viewpoints'])
 inplane_rotations = np.array(example_coco.dataset['inplane_rotations'])
 fixed_transforms_dict = example_coco.dataset['fixed_transforms']
@@ -44,10 +44,10 @@ camera_intrinsics = example_coco.dataset['camera_intrinsic_settings']
 
 print(image_data)
 
-plt.figure()
+# plt.figure()
 img_path = os.path.join(image_directory, image_data['file_name'])
 image = io.imread(img_path)
-plt.imshow(image); plt.axis('off')
+# plt.imshow(image); plt.axis('off')
 
 # # Running model on image
 
@@ -77,20 +77,32 @@ coco_demo = COCODemo(
     camera_intrinsics = camera_intrinsics
 )
 
-
+from mpl_toolkits.axes_grid1 import ImageGrid
 import cv2
 img = cv2.imread(img_path)
 composite, result, img_list = coco_demo.run_on_opencv_image(img)
-plt.figure()
-plt.imshow(composite); plt.axis('off')
-cv2.imwrite('composite.png', composite)
+fig = plt.figure(1, (4., 4.))
+grid = ImageGrid(fig, 111,  
+                 nrows_ncols=(1, len(img_list)+1),
+                 axes_pad=0.1, 
+                 )
+
+# plt.imshow(composite); plt.axis('off')
+# cv2.imwrite('composite.png', composite)
+grid[0].imshow(cv2.cvtColor(composite, cv2.COLOR_BGR2RGB))
+grid[0].axis("off")
 
 for i in range(len(img_list)):
     image_file = os.path.join(
         "{}-color.png".format(i),
     )
     rgb_gl = img_list[i][0]
-    cv2.imwrite(image_file, rgb_gl)
+    # cv2.imwrite(image_file, rgb_gl)
+    grid[i+1].imshow(cv2.cvtColor(rgb_gl, cv2.COLOR_BGR2RGB))
+    grid[i+1].axis("off")
+
+plt.savefig('output.png')
+plt.show()
     # print(i[0])
 
 