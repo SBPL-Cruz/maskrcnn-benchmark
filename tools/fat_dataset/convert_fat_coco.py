running and making custom perch dataset, fixed transforms to table frame, accuracy calculation, code for finding symmetrical axis

tools/fat_dataset/convert_fat_coco.py
@@ -30,58 +30,82 @@ from dipy.core.geometry import cart2sphere, sphere2cart, sphere_distance
 from lib.render_glumpy.render_py import Render_Py
 from lib.utils.mkdir_if_missing import mkdir_if_missing
 
-ROOT_DIR = '/media/aditya/A69AFABA9AFA85D9/Datasets/fat/mixed/extra'
-SCENES = [ \
-        #   "kitchen_0", "kitchen_1", "kitchen_2", "kitchen_3", 
-           "kitchen_4",
-        #    "kitedemo_0", "kitedemo_1", "kitedemo_2", "kitedemo_3", 
-           "kitedemo_4",
-        #    "temple_0", "temple_1", "temple_2", "temple_3", 
-           "temple_4"  
-          ]
-IMAGE_DIR_LIST = [
-        # os.path.join(ROOT_DIR, "kitchen_0"),
-         
-        # os.path.join(ROOT_DIR, "kitchen_1"),
-        # os.path.join(ROOT_DIR, "kitchen_2"),
-        # os.path.join(ROOT_DIR, "kitchen_3"),
-        # os.path.join(ROOT_DIR, "kitchen_4"),
-        # # os.path.join(ROOT_DIR, "kitedemo_0"),
-        # os.path.join(ROOT_DIR, "kitedemo_1"),
-        # os.path.join(ROOT_DIR, "kitedemo_2"),
-        # os.path.join(ROOT_DIR, "kitedemo_3"),
-        # os.path.join(ROOT_DIR, "kitedemo_4"),
-        # # os.path.join(ROOT_DIR, "temple_0"),
-        # os.path.join(ROOT_DIR, "temple_1"),
-        # os.path.join(ROOT_DIR, "temple_2"),
-        # os.path.join(ROOT_DIR, "temple_3"),
-        # os.path.join(ROOT_DIR, "temple_4"),
-        "002_master_chef_can_16k",
-        "003_cracker_box_16k",
-        "004_sugar_box_16k",
-        "005_tomato_soup_can_16k",
-        "006_mustard_bottle_16k",
-        "007_tuna_fish_can_16k",
-        "008_pudding_box_16k",
-        "009_gelatin_box_16k",
-        "010_potted_meat_can_16k",
-        "011_banana_16k",
-        "019_pitcher_base_16k",
-        "021_bleach_cleanser_16k",
-        "024_bowl_16k",
-        "025_mug_16k",
-        "035_power_drill_16k",
-        "036_wood_block_16k",
-        "037_scissors_16k",
-        "040_large_marker_16k",
-        "051_large_clamp_16k",
-        "052_extra_large_clamp_16k",
-        "061_foam_brick_16k",
-        "",
-]
+if False:
+    ROOT_DIR = '/media/aditya/A69AFABA9AFA85D9/Datasets/fat/mixed/extra'
+    SCENES = [ \
+            #   "kitchen_0", "kitchen_1", "kitchen_2", "kitchen_3", 
+            "kitchen_4",
+            #    "kitedemo_0", "kitedemo_1", "kitedemo_2", "kitedemo_3", 
+            # "kitedemo_4",
+            #    "temple_0", "temple_1", "temple_2", "temple_3", 
+            # "temple_4"  
+            ]
+
+    object_settings_file = Path(os.path.join(ROOT_DIR, "kitchen_0", "_object_settings.json"))
+    camera_settings_file = Path(os.path.join(ROOT_DIR, "kitchen_0", "_camera_settings.json"))
+    SYMMETRY_INFO = {
+        "002_master_chef_can_16k" : [0.5, 1 , 0.5],
+        "003_cracker_box_16k" : [0.5, 0.5, 0.5],
+        "004_sugar_box_16k" : [0.5, 0.5, 0.5],
+        "005_tomato_soup_can_16k" : [0.5, 1, 0.5],
+        "006_mustard_bottle_16k" : [0, 0.5, 0],
+        "007_tuna_fish_can_16k" : [0.5, 1, 0.5],
+        "008_pudding_box_16k" : [0.5, 0.5, 0.5],
+        "009_gelatin_box_16k" : [0.5, 0.5, 0.5],
+        "010_potted_meat_can_16k" : [0.5, 0.5, 0.5],
+        "011_banana_16k" : [0, 0, 0.5],
+        "019_pitcher_base_16k" : [0, 0, 0],
+        "021_bleach_cleanser_16k" : [0, 0, 0],
+        "024_bowl_16k" : [0, 1, 0],
+        "025_mug_16k" : [0, 0, 0],
+        "035_power_drill_16k" : [0, 0, 0],
+        "036_wood_block_16k" : [0.5, 0.5, 0.5],
+        "037_scissors_16k" : [0.5, 0, 0],
+        "040_large_marker_16k" : [0.5, 1, 0],
+        "051_large_clamp_16k" : [0.5, 0, 0],
+        "052_extra_large_clamp_16k" : [0.5, 0, 0],
+        "061_foam_brick_16k" : [0.5, 0.5, 0.5],
+    }
+    IMAGE_DIR_LIST = [
+            "002_master_chef_can_16k",
+            # "003_cracker_box_16k",
+            # "004_sugar_box_16k",
+            # "005_tomato_soup_can_16k",
+            # "006_mustard_bottle_16k",
+            # "007_tuna_fish_can_16k",
+            # "008_pudding_box_16k",
+            # "009_gelatin_box_16k",
+            # "010_potted_meat_can_16k",
+            # "011_banana_16k",
+            # "019_pitcher_base_16k",
+            # "021_bleach_cleanser_16k",
+            # "024_bowl_16k",
+            # "025_mug_16k",
+            # "035_power_drill_16k",
+            # "036_wood_block_16k",
+            # "037_scissors_16k",
+            # "040_large_marker_16k",
+            # "051_large_clamp_16k",
+            # "052_extra_large_clamp_16k",
+            # "061_foam_brick_16k",
+            # "",
+    ]
+    OUTFILE_NAME = 'instances_fat_train_pose_symmetry_2018'
+
+if True:
+    ROOT_DIR = '/media/aditya/A69AFABA9AFA85D9/Cruzr/code/Dataset_Synthesizer/Test/Zed'
+    SCENES = [ "NewMap1" ]
+
+    object_settings_file = Path(os.path.join(ROOT_DIR, SCENES[0], "_object_settings.json"))
+    camera_settings_file = Path(os.path.join(ROOT_DIR, SCENES[0], "_camera_settings.json"))
+    IMAGE_DIR_LIST = [
+            "",
+    ]
+    OUTFILE_NAME = 'instances_fat_val_pose_2018'
 
 
-ng = 1000
+
+ng = 642
 print ( '' )
 print ( '  Number of points NG = %d' % ( ng ) )
 
@@ -93,7 +117,6 @@ inplane_rot_angles = np.linspace(-math.pi, math.pi, 68)
 
 # ROOT_OUTDIR = '/media/aditya/A69AFABA9AFA85D9/Datasets/fat/mixed/train'
 # OUTFILE_NAME = 'instances_fat_train_pose_limited_2018'
-OUTFILE_NAME = 'instances_fat_val_pose_2018'
 
 INFO = {
     "description": "Example Dataset",
@@ -114,7 +137,7 @@ LICENSES = [
 LM6d_root = "/media/aditya/A69AFABA9AFA85D9/Datasets/YCB_Video_Dataset/"
 
 def filter_for_jpeg(root, files):
-    file_types = ['*.left.jpeg', '*.left.jpg']
+    file_types = ['*.left.jpeg', '*.left.jpg', '*.left.png']
     file_types = r'|'.join([fnmatch.translate(x) for x in file_types])
     files = [os.path.join(root, f) for f in files]
     files = [f for f in files if re.match(file_types, f)]
@@ -245,6 +268,7 @@ def sphere2euler(theta, phi):
 
 
 def get_object_pose_in_world(object_pose, camera_pose, fat_world_pose=None, type='quat'):
+    # Returns in cm
     object_pose_matrix = np.zeros((4,4))
     object_pose_matrix[:3,:3] = RT_transform.quat2mat(get_wxyz_quaternion(object_pose['quaternion_xyzw']))
     object_pose_matrix[:,3] = object_pose['location'] + [1]
@@ -314,13 +338,33 @@ def get_segmentation_data_for_scene(IMG_DIR):
         raise Exception("Object settings file not found")
     return SEGMENTATION_DATA
 
+def apply_angle_symmetry(angles, symmetry_info):
+    new_angles = []
+    print(angles)
+    for i in range(3):
+        # if symmetry_info[i] == 0.5:
+        #     if angles[i] < 0:
+        #         print("Semi symmetric, beyond 180 angle")
+        #         new_angles.append(angles[i] + np.pi)
+        #     else:
+        #         print("Semi symmetric, within 180 angle")
+        #         new_angles.append(angles[i])
+        if symmetry_info[i] == 1:
+            new_angles.append(0)
+        else:
+            new_angles.append(angles[i])
+
+        # if symmetry_info[i] == 0:
+        #     new_angles.append(angles[i])
+    return np.array(new_angles)
+        
+
+
 def main():
 
     # object_settings_file = Path(os.path.join(IMAGE_DIR_LIST[0], "_object_settings.json"))
     # camera_settings_file = Path(os.path.join(IMAGE_DIR_LIST[0], "_camera_settings.json"))
 
-    object_settings_file = Path(os.path.join(ROOT_DIR, "kitchen_0", "_object_settings.json"))
-    camera_settings_file = Path(os.path.join(ROOT_DIR, "kitchen_0", "_camera_settings.json"))
     if object_settings_file.is_file():
         with open(object_settings_file) as file:
             object_settings_data = json.load(file)
@@ -381,6 +425,7 @@ def main():
             image_files = filter_for_jpeg(IMAGE_DIR, all_dir_files)
             # dir_name = os.path.basename(IMAGE_DIR)
             SEGMENTATION_DATA = get_segmentation_data_for_scene(IMAGE_DIR)
+            print(SEGMENTATION_DATA)
             # go through each image
             for ii in trange(len(image_files)):
                 image_filename = image_files[ii]
@@ -409,7 +454,8 @@ def main():
                 # print("File %d - %s"% (image_global_id, label_filename))
                 my_file = Path(label_filename)
                 segmentation_image = skimage.io.imread(segmentation_image_files[0])
-
+                # print("File %d - %s"% (image_global_id, segmentation_image_files[0]))
+                
                 if my_file.is_file():
                     with open(label_filename) as file:
                         label_data = json.load(file)
@@ -420,6 +466,7 @@ def main():
                             quat = label_data['objects'][i]['quaternion_xyzw']
                             
                             angles = RT_transform.quat2euler(get_wxyz_quaternion(quat))
+                            # angles = apply_angle_symmetry(angles, SYMMETRY_INFO[class_name])
                             # This function gives angles with this convention of euler - https://en.wikipedia.org/wiki/Euler_angles#Signs_and_ranges (geometric definition)
 
                             # if np.isclose(angles[1], 0):

tools/fat_dataset/visualize_fat_coco.py
@@ -70,12 +70,14 @@ def render_pose(rendered_dir, count, class_name, fixed_transforms_dict, camera_i
 
     depth_gl = (depth_gl * depth_factor).astype(np.uint16)
 
-    cv2.imwrite(image_file, rgb_gl)
-    cv2.imwrite(depth_file, depth_gl)
+    return rgb_gl, depth_gl
+    # cv2.imwrite(image_file, rgb_gl)
+    # cv2.imwrite(depth_file, depth_gl)
 
 
 image_directory = '/media/aditya/A69AFABA9AFA85D9/Datasets/fat/mixed/extra/'
-annotation_file = '/media/aditya/A69AFABA9AFA85D9/Datasets/fat/mixed/extra/instances_fat_train_pose_2018.json'
+# annotation_file = '/media/aditya/A69AFABA9AFA85D9/Datasets/fat/mixed/extra/instances_fat_train_pose_2018.json'
+annotation_file = '/media/aditya/A69AFABA9AFA85D9/Datasets/fat/mixed/extra/instances_fat_train_pose_symmetry_2018.json'
 
 example_coco = COCO(annotation_file)
 camera_intrinsics = example_coco.dataset['camera_intrinsic_settings']
@@ -92,10 +94,10 @@ print('Custom COCO supercategories: \n{}'.format(' '.join(category_names)))
 
 category_ids = example_coco.getCatIds(catNms=['square'])
 image_ids = example_coco.getImgIds(catIds=category_ids)
-print(len(image_ids))
-# image_data = example_coco.loadImgs(image_ids[np.random.randint(0, len(image_ids))])[0]
+print("Number of images : {}".format(len(image_ids)))
+image_data = example_coco.loadImgs(image_ids[np.random.randint(0, len(image_ids))])[0]
 # image_data = example_coco.loadImgs(image_ids[8])[0]
-image_data = example_coco.loadImgs(image_ids[0])[0]
+# image_data = example_coco.loadImgs(image_ids[0])[0]
 
 print(image_data)
 directory = './output'
@@ -106,13 +108,15 @@ else:
     os.makedirs(directory)
 
 plt.figure()
+plt.axis("off")
+plt.plot([3,1,4])
 image = io.imread(image_directory + image_data['file_name'])
-io.imsave(os.path.join(directory, 'original.png'), image)
+# io.imsave(os.path.join(directory, 'original.png'), image)
 
-plt.imshow(image); plt.axis('off')
+# plt.imshow(image); plt.axis('off')
 
-plt.figure()
-plt.imshow(image); plt.axis('off')
+# plt.figure()
+# plt.imshow(image); plt.axis('off')
 pylab.rcParams['figure.figsize'] = (8.0, 10.0)
 annotation_ids = example_coco.getAnnIds(imgIds=image_data['id'], catIds=category_ids, iscrowd=None)
 annotations = example_coco.loadAnns(annotation_ids)
@@ -134,7 +138,7 @@ for annotation in annotations:
     print("Recovered rotation : {}".format(xyz_rotation_angles))
     quat = annotation['quaternion_xyzw']
     print("Actual rotation : {}".format(RT_transform.quat2euler(get_wxyz_quaternion(quat))))
-    render_pose(directory, count, class_name, fixed_transforms_dict, 
-                camera_intrinsics, annotation['camera_pose'], xyz_rotation_angles, annotation['location'], annotation['quaternion_xyzw'])
+    rgb, depth = render_pose(directory, count, class_name, fixed_transforms_dict, 
+                    camera_intrinsics, annotation['camera_pose'], xyz_rotation_angles, annotation['location'], annotation['quaternion_xyzw'])
     count += 1
 plt.show()